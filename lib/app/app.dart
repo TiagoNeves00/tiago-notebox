656d2df3e81bd8c71c75c374b5c7bda8490142ff
TiagoNeves00 <tcaldeiraneves@gmail.com>
2025-09-10 22:56:20 +0100

    Type of Letter
    Apply other type of letter via google_fonts for a friendlier look

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,11 +1,10 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:notebox/app/router.dart';
 import 'package:notebox/theme/app_colors.dart';
 import 'package:notebox/theme/theme_mode.dart';
-
-
 
 class NoteBoxApp extends ConsumerWidget {
   const NoteBoxApp({super.key});
@@ -13,27 +12,35 @@ class NoteBoxApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final mode = ref.watch(themeModeProvider);
-    return DynamicColorBuilder(builder: (lightDyn, darkDyn) {
-      final light = ThemeData(
-        useMaterial3: true,
-        colorScheme: lightDyn ??
-            ColorScheme.fromSeed(seedColor: AppColors.light.brand),
-        extensions: const [AppColors.light],
-      );
-      final dark = ThemeData(
-        useMaterial3: true,
-        colorScheme: darkDyn ??
-            ColorScheme.fromSeed(
-              seedColor: AppColors.dark.brand,
-              brightness: Brightness.dark,
-            ),
-        extensions: const [AppColors.dark],
-      );
-      return MaterialApp.router(
-        title: 'NoteBox',
-        theme: light, darkTheme: dark, themeMode: mode,
-        routerConfig: appRouter,
-      );
-    });
+    return DynamicColorBuilder(
+      builder: (lightDyn, darkDyn) {
+        final light = ThemeData(
+          useMaterial3: true,
+          colorScheme:
+              lightDyn ??
+              ColorScheme.fromSeed(seedColor: AppColors.light.brand),
+          textTheme: GoogleFonts.nunitoTextTheme(),
+          extensions: const [AppColors.light],
+        );
+        final dark = ThemeData(
+          useMaterial3: true,
+          colorScheme:
+              darkDyn ??
+              ColorScheme.fromSeed(
+                seedColor: AppColors.dark.brand,
+                brightness: Brightness.dark,
+              ),
+          textTheme: GoogleFonts.nunitoTextTheme(ThemeData.dark().textTheme),
+          extensions: const [AppColors.dark],
+        );
+        return MaterialApp.router(
+          title: 'NoteBox',
+          theme: light,
+          darkTheme: dark,
+          themeMode: mode,
+          routerConfig: appRouter,
+        );
+      },
+    );
   }
 }
